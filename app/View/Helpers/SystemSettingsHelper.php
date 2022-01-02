19d00c072efb4d2866cc54491b2750e620c5fe24
Olie <oliever@gmail.com>
2022-01-02 12:38:01 +0800

    Added SystemSettingsHelper

---
+++ app/View/Helpers/SystemSettingsHelper.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Models\SystemSetting;
+use Carbon\Carbon;
+
+/*Add as autoloadedd file on composer.json
+* run composer dump-autoload
+*/
+
+if(!function_exists('settingsValue')){
+    function settingsValue($code){
+
+        $s = SystemSetting::where(['team_id'=> auth()->user()->currentTeam->id,'code'=>$code])->first();
+        return $s->value;
+    }
+}
