e46fc1dfcbb58630d6cf8693710e9073240d2214
Olie <oliever@gmail.com>
2022-01-12 17:56:01 +0800

    hide footer for now

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="py-2 md:flex md:items-center md:justify-center border-t-2 border-gray-200 dark:border-gray-800" >
+<div class="py-2 md:items-center md:justify-center border-t-2 border-gray-200 dark:border-gray-800 hidden" >
     <ul class="">
         <li class="md:mx-2 md:inline leading-7 text-sm" id="footer-navi-2"><a class="text-gray-700 dark:text-gray-400 underline text-small" href="/disclaimer">Disclaimer</a></li>
         <li class="md:mx-2 md:inline leading-7 text-sm" id="footer-navi-2"><a class="text-gray-700 dark:text-gray-400 underline text-small" href="/cookie">Cookie policy</a></li>
