780bb53f1606005ebed12ea56636f0d02bbd24b4
Olie <oliever@gmail.com>
2022-02-03 14:09:50 +0800

    added laravel component for boolean icon

---
+++ resources/views/components/icons/boolean_yes.blade.php
@@ -0,0 +1,3 @@
+<svg class="h-5 w-5 stroke-current text-green-600 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+</svg>
